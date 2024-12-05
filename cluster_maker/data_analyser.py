bebd092154262b6f702ba66b9c0ae9c46facc2d5
Adam Young <adam.young.com@gmail.com>
2024-12-05 12:42:02 +0000

    Added doc strings to the two functions in 'data_analyser.py'

--- cluster_maker/data_analyser.py
+++ cluster_maker/data_analyser.py
@@ -7,10 +7,26 @@
 ###
 
 ## Import necessary libraries
-import pandas as pd
+try:
+    import pandas as pd
+except ImportError:
+    raise ImportError("*** ERROR: You do not currently have Pandas installed. Please install it to use this code ***")
 
 ## Function to calculate the correlation matrix
 def calculate_correlation(data):
+    """
+    This function computes the correlation between each pair of columns in the provided 
+    DataFrame to then calculate the correlation matrix of a given DataFrame. 
+    
+    Parameters:
+        1 > data: The DataFrame containing the data. 
+                  The columns in this DataFrame should be numerical.
+
+    Returns:
+        1 > correlation_matrix: a DataFrame representing the correlation matrix. Each cell in the matrix 
+                                contains the correlation coefficient between two columns of the input DataFrame.
+    """
+
     # Calculate the correlation matrix
     correlation_matrix = data.corr()
         
@@ -19,6 +35,22 @@ def calculate_correlation(data):
     
 ## Function to calculate descriptive statistics of data
 def calculate_descriptive_statistics(data):
+    """
+    This function calculates descriptive statistics for each column in the DataFrame, Iincluding count, mean, 
+    standard deviation, min, max, and percentiles (25%, 50%, 75%). Additionally, it calculates the number of missing 
+    values for each column.
+
+    Parameters:
+        1 > data: The DataFrame containing the data. 
+                  The columns in this DataFrame should be numerical.
+
+    Returns:
+        1 > stats: A DataFrame containing descriptive statistics for each column in 
+                   the input DataFrame, including count, mean, std, min, max, and percentiles, 
+                   along with the count of missing values for each column.
+    
+    """
+
     # Calculate descriptive statistics
     stats = data.describe(include='all').T
     stats['missing_values'] = data.isnull().sum()
