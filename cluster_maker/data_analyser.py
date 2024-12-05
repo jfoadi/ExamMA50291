46daf229f89e1e390a721275dc0da0d685f5a6ac
Pelle Strandberg <pelle.strandberg1@gmail.com>
2024-12-05 14:15:14 +0000

    Some minor changes

--- cluster_maker/data_analyser.py
+++ cluster_maker/data_analyser.py
@@ -13,20 +13,25 @@ import pandas as pd
 def calculate_correlation(data):
     '''Goal: Using pandas library to Calculate Correlation Matrix of a given data.
       Function: calculate_correlation(data): 
-      - this function takes a pandas dataframe as input and returns covariance matrix of the data. 
+      - this function takes a pandas dataframe as input and returns the covariance matrix of the data. 
       
       Parameters: 
       - data: input data which is used to calculate covariance matrix. 
        
       Returns:
       - correlation_matrix: correlation matrix of the input data. '''
-    # Calculate the correlation matrix
+    # Calculate the correlation matrix (with included excpetion handling)
     try: 
+        if not isinstance(data, pd.DataFrame):
+            raise TypeError('Input data must be a pandas DataFrame')
         correlation_matrix = data.corr()
         return correlation_matrix
     # Return the correlation matrix
-    except Exception as e: 
+    except (ValueError, TypeError) as e: 
         print(f'An error occured when calculating the correlation matrix:{e}')
+        return None
+    except Exception as e: 
+        print('An unexcpected error occured: {e}')
         return None
     
 ## Function to calculate descriptive statistics of data
@@ -36,20 +41,24 @@ def calculate_descriptive_statistics(data):
       - This function takes the data input and returns the descriptive statistics of the data. 
       
       Parameters: 
-      - data: input data which is used to calculate or display the descriptive statistics. 
+      - data: input data which is used to calculate and display the descriptive statistics. 
       
       Returns: 
       - stats: Returns descriptive statistics of the input data.
       '''
 
-    # Calculate descriptive statistics
+    # Calculate descriptive statistics (with included exception handling)
     try: 
+        if not isinstance(data, pd.DataFrame):
+            raise TypeError('Input data must be a pandas DataFrame')
         stats = data.describe(include='all').T
         stats['missing_values'] = data.isnull().sum()
-            
         return stats
+    except (ValueError, TypeError) as e: 
+        print(f'An erro occured when calculating the descriptive statistics: {e}')
+        return None
     except Exception as e: 
-        print(f'An error occured when calculating and displaying the descriptive statistics: {e}')
+        print(f'An unexpected error occured: {e}')
         return None
     
 
