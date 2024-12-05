02fd4f710d64b02f4819ae7fdf707352f3f8baa4
Amar <asr205@bath.ac.uk>
2024-12-05 12:43:31 +0000

    added relevant exception handling to both functions in data analyser

--- cluster_maker/data_analyser.py
+++ cluster_maker/data_analyser.py
@@ -20,11 +20,29 @@ def calculate_correlation(data):
     The function returns a DataFrame of the correlation matrix corresponding to the input data
 
     """
-    # Calculate the correlation matrix
-    correlation_matrix = data.corr()
+    try:
+        # Checking that data provided is a dataframe.
+        if not isinstance(data, pd.DataFrame):
+            raise TypeError("The input provided is not a DataFrame")
+        # Checking that the data is not empty
+        if data.empty:
+            raise ValueError("The Data has no numerical data")
         
-    # Return the correlation matrix
-    return correlation_matrix
+        # Calculate the correlation matrix
+        correlation_matrix = data.corr()
+        
+        # Return the correlation matrix
+        return correlation_matrix
+
+    except TypeError as e:
+        print(f"Error calculating correlation: {e}")
+        return None
+    except ValueError as e: 
+        print(f"Error calculating correlation: {e}")
+        return None
+    except Exception as e:
+        print(f"There was an unexpected error: {e}")
+        return None
     
 ## Function to calculate descriptive statistics of data
 def calculate_descriptive_statistics(data):
@@ -39,8 +57,26 @@ def calculate_descriptive_statistics(data):
 
 
     """
-    # Calculate descriptive statistics
-    stats = data.describe(include='all').T
-    stats['missing_values'] = data.isnull().sum()
+    try:
+        # Check that data provided is a dataframe
+        if not isinstance(data, pd.DataFrame):
+            raise TypeError("The input provided is not a DataFrame")
+        # Check that the data is not empty
+        if data.empty:
+            raise ValueError("The Data has no numerical data")
         
-    return stats
+        # Calculate descriptive statistics
+        stats = data.describe(include='all').T
+        stats['missing_values'] = data.isnull().sum()
+            
+        return stats
+    
+    except TypeError as e:
+        print(f"Error calculating statistics: {e}")
+        return None
+    except ValueError as e: 
+        print(f"Error calculating statistics: {e}")
+        return None
+    except Exception as e:
+        print(f"There was an unexpected error: {e}")
+        return None
