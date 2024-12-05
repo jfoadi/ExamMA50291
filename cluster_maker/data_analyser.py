5cb677836aabd0c7a02d46f7ab005632255a3ca0
Maria Vadan <mcv32@bath.ac.uk>
2024-12-05 12:33:23 +0000

    Added docstrings to functions in data_analyser.py

--- cluster_maker/data_analyser.py
+++ cluster_maker/data_analyser.py
@@ -11,6 +11,16 @@ import pandas as pd
 
 ## Function to calculate the correlation matrix
 def calculate_correlation(data):
+    """
+    Calculate the correlation matrix of a dataframe.
+
+    Parameters:
+        data (pd.DataFrame): The DataFrame to compute the pairwise correlation of its columns.
+
+    Returns:
+        pd.DataFrame: DataFrame containing the correlation matrix of the input data.
+
+    """
     # Calculate the correlation matrix
     correlation_matrix = data.corr()
         
@@ -19,6 +29,17 @@ def calculate_correlation(data):
     
 ## Function to calculate descriptive statistics of data
 def calculate_descriptive_statistics(data):
+    """
+    Generate the descriptive statistics of a dataframe.
+
+    Parameters:
+        data (pd.DataFrame): The DataFrame to calculate the descriptive statistics of its columns.
+
+    Returns:
+        pd.DataFrame: DataFrame containing the descriptive statistics of the input data columns and the number of missing values.
+        Columns:
+            column  | count  |  mean  |  std  |  min  |  25%  |  50%  |  75%  |  max  |  missing_values
+    """
     # Calculate descriptive statistics
     stats = data.describe(include='all').T
     stats['missing_values'] = data.isnull().sum()
