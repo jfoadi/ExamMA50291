6078e8b0f5150e9c400b65c1ae0a157acec126dc
Amar <asr205@bath.ac.uk>
2024-12-05 13:06:49 +0000

    removed duplication in test2_cm.py and creared demo file

--- test2_cm.py
+++ test2_cm.py
@@ -6,14 +6,6 @@
 ## Import cluster_maker
 import cluster_maker as cm
 
-###
-## To test cluster_maker
-## J. Foadi - University of Bath - 2024
-###
-
-## Import cluster_maker
-import cluster_maker as cm
-
 ## Main
 if __name__ == '__main__':
     # Create input for define_dataframe_structure
@@ -29,15 +21,11 @@ if __name__ == '__main__':
 
     # Simulate 20 data points per group
     data = cm.simulate_data(df, 20)
-    print(data)
 
     # Try this first
     try:
         crr = cm.calculate_correlation(data)
-        print(crr)
-        print("success")
     except AttributeError as a:
-        print("passed")
         pass
 
     # Conclusion
