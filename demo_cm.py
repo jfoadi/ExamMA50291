2b2fd93b566db3c414f41216ba3d8bce05face15
kad02 <b.kaden.02@gmail.com>
2024-12-05 14:01:01 +0000

    Checked task 6

--- demo_cm.py
+++ demo_cm.py
@@ -10,11 +10,13 @@ import matplotlib.pyplot as plt
 
 # Example to demonstrate the use of the simulate_data function, using the define_dataframe_structure output as input
 print("Example to demonstrate the use of the simulate_data function, using the define_dataframe_structure output as input\n")
+
+# Define data as age and income
 data = [{
     'name':'age', 'reps': [25, 30, 35]},
     {'name': 'income', 'reps': [50000, 60000, 70000]
 }]
-
+# Use the define_dataframe_structure function to create a seed data frame
 seed_df = cm.define_dataframe_structure(data)
 
 print(f'Seed data frame from define_dataframe_structure function:\n{seed_df}')
@@ -40,6 +42,8 @@ col_specs = {
         'variance': 5.0
     }}
 
+# Use the simulate_data function to generate new data based on the seed data frame
+# Should generate 10 points per group
 new_df = cm.simulate_data(seed_df, n_points=10, col_specs=col_specs)
 
 print(f'Data frame after simulate_data function:\n{new_df}')
@@ -49,41 +53,5 @@ plt.scatter(new_df['age'], new_df['income'])
 plt.xlabel('Age')
 plt.ylabel('Income')
 plt.title('Simulated Data Frame')
-plt.grid(True)
-plt.show()
-
-
-
-
-## Example to demonstrate the use of intelligent_clusters function
-print("\nExample to demonstrate the use of intelligent_clusters function\n")
-data = [{
-    'name':'age', 'reps': [25, 30, 35]},
-    {'name': 'income', 'reps': [50040, 60200, 60000]
-}]
-
-seed_df = cm.define_dataframe_structure(data)
-
-separation = 5
-col_specs = {
-    'age': {
-        'distribution': 'normal',
-        'variance': 4.0
-    },
-    'income': {
-        'distribution': 'uniform',
-        'variance': 5000
-    }
-}
-n_groups = 3
-n_points = 100
-random_state = 42
-
-separated_groups = cm.sepatared_groups(seed_df, separation,col_specs,n_groups,n_points, random_state=random_state)
-
-plt.scatter(separated_groups['age'], separated_groups['income'])
-plt.xlabel('Age')
-plt.ylabel('Income')
-plt.title('Separated Groups')
 plt.grid(True)
 plt.show()
