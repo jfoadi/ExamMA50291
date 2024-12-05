6836ad0d9d48a3a108240a15f6653c03b5c19035
Pelle Strandberg <pelle.strandberg1@gmail.com>
2024-12-05 12:39:55 +0000

    Second commit after changes of question 3 and 4

--- cluster_maker/data_analyser.py
+++ cluster_maker/data_analyser.py
@@ -11,16 +11,46 @@ import pandas as pd
 
 ## Function to calculate the correlation matrix
 def calculate_correlation(data):
+    '''Goal: Using pandas module to Calculate Correlation Matrix and descriptive statistics of a given data.
+      Function: calculate_correlation(data): 
+      - this function takes a pandas dataframe as input and returns covariance matrix of the data. 
+      
+      Parameters: 
+      - data: input data which is used to calculate covariance matrix. 
+       
+      Returns:
+      - correlation)matrix: correlation matrix of the input data. '''
     # Calculate the correlation matrix
-    correlation_matrix = data.corr()
-        
+    try: 
+        correlation_matrix = data.corr()
+        return correlation_matrix
     # Return the correlation matrix
-    return correlation_matrix
+    except Exception as e: 
+        print(f'An error occured when calculating the correlation matrix:{e}')
+        return None
     
 ## Function to calculate descriptive statistics of data
 def calculate_descriptive_statistics(data):
+    '''Goal: Using pandas module and input data to calculate descriptive statistics of the given data
+      Function: calculate_descriptive_statistics(data):
+      - This function takes the data input and returns the descriptive statistics of the data. 
+      
+      Parameters: 
+      - data: input data which is used to calculate or display the descriptive statistics. 
+      
+      Returns: 
+      - stats: Returns descriptive statistics of the input data.
+      '''
+
     # Calculate descriptive statistics
-    stats = data.describe(include='all').T
-    stats['missing_values'] = data.isnull().sum()
-        
-    return stats
+    try: 
+        stats = data.describe(include='all').T
+        stats['missing_values'] = data.isnull().sum()
+            
+        return stats
+    except Exception as e: 
+        print(f'An error occured when calculating and displaying the descriptive statistics: {e}')
+        return None
+    
+
+
