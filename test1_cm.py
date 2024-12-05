1bfa6648daff0400d269f9566ca1f7087e47bc58
Tom Williams <tlw54@bath.ac.uk>
2024-12-05 14:28:18 +0000

    Created demo_cm.py

--- test1_cm.py
+++ test1_cm.py
@@ -8,9 +8,9 @@ import cluster_maker as cm
 
 ## Create input for define_dataframe_structure
 column_specs = [
-    {'name': "height", 'reps': [180, 160, 120]},
-    {'name': "weight", 'reps': [80, 60, 30]},
-    {'name': "age", 'reps': [40, 35, 10]}
+    {'name': 'height', 'reps': [180, 160, 120]},
+    {'name': 'weight', 'reps': [80, 60, 30]},
+    {'name': 'age', 'reps': [40, 35, 10]}
 ]
 
 df = cm.define_dataframe_structure(column_specs)
