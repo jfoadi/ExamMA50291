2f23561537b6e383f3ff5301dab993a035a8aa21
Maria Vadan <mcv32@bath.ac.uk>
2024-12-05 12:59:49 +0000

    Added exception handling for functions in data_analyser.py

--- cluster_maker/data_analyser.py
+++ cluster_maker/data_analyser.py
@@ -21,12 +21,28 @@ def calculate_correlation(data):
         pd.DataFrame: DataFrame containing the correlation matrix of the input data.
 
     """
-    # Calculate the correlation matrix
-    correlation_matrix = data.corr()
+    # Exception handling for input type
+    try:
+        # check if input is a pandas DataFrame
+        if not isinstance(data, pd.DataFrame):
+            raise TypeError("data must be a pandas DataFrame")
+        # check if data is not empty
+        if data.empty:
+            raise ValueError("data must not be an empty DataFrame")
+        # check if DataFrame has non-numeric columns
+        if not data.select_dtypes(include='number').columns.tolist():
+            raise ValueError("data must contain numeric columns to calculate correlation matrix")
         
-    # Return the correlation matrix
-    return correlation_matrix
+        # Calculate the correlation matrix
+        correlation_matrix = data.corr()
+            
+        # Return the correlation matrix
+        return correlation_matrix
     
+    except Exception as e:
+        print(f"Error calculating correlation matrix: {e}")
+    
+
 ## Function to calculate descriptive statistics of data
 def calculate_descriptive_statistics(data):
     """
@@ -40,8 +56,22 @@ def calculate_descriptive_statistics(data):
         Columns:
             column  | count  |  mean  |  std  |  min  |  25%  |  50%  |  75%  |  max  |  missing_values
     """
-    # Calculate descriptive statistics
-    stats = data.describe(include='all').T
-    stats['missing_values'] = data.isnull().sum()
+    try:
+        # check if input is a pandas DataFrame
+        if not isinstance(data, pd.DataFrame):
+            raise TypeError("data must be a pandas DataFrame")
+        # check if data is not empty
+        if data.empty:
+            raise ValueError("data must not be an empty DataFrame")
+        # check if DataFrame has non-numeric columns
+        if not data.select_dtypes(include='number').columns.tolist():
+            raise ValueError("data must contain numeric columns to calculate descriptive statistics")
         
-    return stats
+        # Calculate descriptive statistics
+        stats = data.describe(include='all').T
+        stats['missing_values'] = data.isnull().sum()
+            
+        return stats
+    
+    except Exception as e:
+        print(f"Error calculating descriptive statistics: {e}")
